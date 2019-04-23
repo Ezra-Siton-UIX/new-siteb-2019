81d85b07d0d8e1b5f6bfc668bb9dc9aaf2754697
Ezra-Siton-UIX <siton22@hotmail.com>
2019-04-23 15:38:55 +0300

    first commit

--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -1,12 +1,14 @@
 <template>
 <Layout>
   <h1>Blog</h1>
+
   <ul class="posts">
     <li v-for="edge in $page.posts.edges" :key="edge.node.id">
       <article>
         <h2>{{ edge.node.title }}</h2>
         <div>{{ edge.node.date }}</div>
         <p>{{ edge.node.description }}</p>
+        <g-image quality="100" width="200" class="responsive" :src="edge.node.image" fit="contain" />
         <g-link :to="edge.node.path">Read Article</g-link>
       </article>
     </li>
@@ -22,6 +24,7 @@
           title
           date (format: "D. MMMM YYYY")
           description
+          image
           path
         }
       }
@@ -36,3 +39,18 @@ export default {
   }
 }
 </script>
+
+
+<style>
+img.responsive{
+  width:100%;
+  height: auto;
+}
+.layout {
+  max-width: 1220px;
+  margin: 0 auto;
+  padding-left: 20px;
+  overflow: hidden;
+  padding-right: 20px;
+}
+</style>
