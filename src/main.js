88eaf0c24bc92b8f5bc71c48106a0ea1dcfcfd0a
Ezra-Siton-UIX <siton22@hotmail.com>
2019-04-25 22:58:15 +0300

    `wow`

--- src/main.js
+++ src/main.js
@@ -45,7 +45,11 @@ export default function(Vue, {
   )
   // Add an external Script Before Body
   head.script.push({
-    src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.4/js/uikit2.min.js',
+    src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.4/js/uikit.min.js',
+    body: true
+  },
+  {
+    src: 'https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.4/js/uikit-icons.min.js',
     body: true
   })
 }
