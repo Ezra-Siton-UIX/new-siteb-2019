fbb823f863820a5177d7e08ef7027f9c949cf32c
Ezra-Siton-UIX <siton22@hotmail.com>
2019-04-25 23:22:02 +0300

    `wow`

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -299,16 +299,6 @@
 <script>
 export default {
   metaInfo: {
-    script: [{
-        src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
-        body: true
-      },
-      {
-        src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
-        body: true
-      }
-
-    ],
     title: 'About us',
     meta: [{
       name: 'author',
