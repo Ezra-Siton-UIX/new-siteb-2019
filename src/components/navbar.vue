d351be3fc8cdbd165bd11e6605a6ad212f7268f0
Ezra-Siton-UIX <siton22@hotmail.com>
2019-05-14 02:25:57 +0300

    ad2d

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -3,7 +3,7 @@
   <nav class="uk-navbar-container uk-position-top uk-navbar-transparent" style="z-index:5;" uk-navbar>
     <div class="uk-navbar-left white">
       <g-link title="Back To Homepage" class="uk-navbar-item uk-logo" to="/">
-        <g-image  src="~/assets/logo.png" width="170" style="width:130px;" />
+        <g-image  blur="0" src="~/assets/logo.png" width="170" style="width:130px;" />
         <span class="uk-hidden">{{ $static.metaData.siteName }}</span>
       </g-link>
     </div>
