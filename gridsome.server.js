7855dc37fed359f8ab84aebf4b81912e7d295df8
Ezra-Siton-UIX <siton22@hotmail.com>
2019-07-29 22:30:49 +0300

    wow

--- gridsome.server.js
+++ gridsome.server.js
@@ -10,3 +10,7 @@ module.exports = function (api) {
     // Use the Data store API here: https://gridsome.org/docs/data-store-api
   })
 }
+api.createPages(({ createPage }) => {
+  // Use the Pages API here: https://gridsome.org/docs/pages-api
+})
+}
